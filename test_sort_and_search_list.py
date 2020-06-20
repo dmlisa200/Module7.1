1e00aa3d1586b33f8ba9d4c3468c33ce020de6e3
dmlisa200 <17917202+dmlisa200@users.noreply.github.com>
2020-06-20 16:18:28 -0500

    Add files via upload

--- test_sort_and_search_list.py
+++ test_sort_and_search_list.py
@@ -1,10 +1,14 @@
 import unittest
 from fun_with_collections.sort_and_search_list import search_list
-
+from fun_with_collections.sort_and_search_list import sort_list
 
 class MyTestCase(unittest.TestCase):
     def test_search_list(self):
         self.assertEqual(search_list(33), 4)
+
+class MyTestClass(unittest.TestCase):
+    def test_sort_list(self):
+        self.assertEqual(sort_list(list), list)
 
 
 if __name__ == '__main__':
